0ba31c2a7c7eaff16e797599979d70f20a6ca645
张鹤鸣 <zhangheming@zhangxiaoxuns-MacBook-Pro.local>
2021-01-22 10:21:07 +0800

    [add] basic router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,14 +9,19 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
+    redirect: {
+      name: 'helloworld',
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/helloworld',
+    name: 'helloworld',
+    component: () => import(/* webpackChunkName: "about" */ '../components/HelloWorld.vue'),
+  },
+  {
+    path: '/*',
+    name: 'notfound',
+    component: () => import(/* webpackChunkName: "about" */ '../views/common/NotFound.vue'),
   },
 ];
 
