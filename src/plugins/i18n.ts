4b1863456c3c87bd4bc9e65f649ad5811dc68993
张鹤鸣 <zhangheming@zhangxiaoxuns-MacBook-Pro.local>
2021-01-26 15:52:46 +0800

    [add] routers

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
-import ch from '@/locale/zh-CN.json';
-import en from '@/locale/en-US.json';
+import ch from '@/locales/zh-CN.json';
+import en from '@/locales/en-US.json';
 
 Vue.use(VueI18n);
 
