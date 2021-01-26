e44e72e8a0a5175f8a66ab9d5b7f7e78a5b5a688
张鹤鸣 <zhangheming@zhangxiaoxuns-MacBook-Pro.local>
2021-01-26 13:50:35 +0800

    [add] navigation config & router in json

--- src/views/master/MasterNavigation.vue
+++ src/views/master/MasterNavigation.vue
@@ -1,20 +1,19 @@
 <template>
   <el-col :span="12">
-    <el-menu
-      class="el-menu-vertical-demo ct-sidebar"
-    >
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">{{ $i18n.t('navigation.info') }}</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+    <el-menu class="el-menu-vertical-demo ct-sidebar" >
+      <aside
+        v-for="(value, index) in $i18n.t('navigation')"
+        :key="index"
+      >
+        <router-link :to=" { name: value.routerName }">
+          <el-menu-item :index="index">
+            <i :class="value.icon" />
+            <span slot="title">
+              {{ value.name }}
+            </span>
+          </el-menu-item>
+        </router-link>
+      </aside>
     </el-menu>
   </el-col>
 </template>
