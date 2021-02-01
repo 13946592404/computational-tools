9102a3fe8ada2d40252598cde354c3cd84c8ed86
张鹤鸣 <zhangheming@zhangxiaoxuns-MacBook-Pro.local>
2021-02-01 14:12:46 +0800

    [fix] i18n observer typescript

--- src/data/requirement.ts
+++ src/data/requirement.ts
@@ -1,15 +1,13 @@
 import i18n from '@/plugins/i18n';
-import VueI18n from 'vue-i18n';
-
-const requirement: VueI18n.TranslateResult = i18n.t('requirement');
 
 // @ts-ignore
-requirement.forEach((value: any, index: number) => {
-  value.index = (index + 1).toString();
-  value.children.forEach((val: object, indx: number) => {
-    // @ts-ignore
-    val.index = `${value.index}-${indx + 1}`;
-  });
-})
+const requirement: array<any> = Array.from(i18n.t('requirement'));
+
+for (let i = 0; i < requirement.length; i += 1) {
+  requirement[i].index = (i + 1).toString();
+  for (let j = 0; j < requirement[i].children.length; j += 1) {
+    requirement[i].children[j].index = `${i + 1}-${j + 1}`;
+  }
+}
 
 export default requirement;
