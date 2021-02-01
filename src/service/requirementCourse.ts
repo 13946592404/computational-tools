ef876d8b483bd461df2ed1ae821a95b5e19fd20a
张鹤鸣 <zhangheming@zhangxiaoxuns-MacBook-Pro.local>
2021-02-01 13:29:24 +0800

    [add] temp sql data

---
+++ src/service/requirementCourse.ts
@@ -0,0 +1,5 @@
+import { courses } from './temp';
+
+export const getCourses = courses;
+
+export default [];
---
+++ src/service/temp.ts
@@ -0,0 +1,23 @@
+export const courses = [
+  { father: '1-1', name: '高等数学A', percent: '15%' },
+  { father: '1-1', name: '线性代数A', percent: '5%' },
+  { father: '1-1', name: '离散数学', percent: '5%' },
+  { father: '1-1', name: '概率论与数理统计', percent: '10%' },
+  { father: '1-1', name: '大学物理B', percent: '5%' },
+  { father: '1-1', name: '计算机导论', percent: '4%' },
+  { father: '1-1', name: '编程导论', percent: '4%' },
+  { father: '1-1', name: '数据结构', percent: '6%' },
+  { father: '1-1', name: '计算机组成与结构', percent: '6%' },
+  { father: '1-1', name: '操作系统', percent: '6%' },
+  { father: '1-1', name: '数据库系统原理', percent: '5%' },
+  { father: '1-1', name: '人工智能', percent: '5%' },
+  { father: '1-1', name: '计算机网络', percent: '5%' },
+  { father: '1-1', name: '编译原理', percent: '5%' },
+  { father: '1-1', name: '嵌入式系统原理', percent: '5%' },
+  { father: '1-1', name: '信息系统安全', percent: '4%' },
+  { father: '1-1', name: '面向对象程序设计', percent: '5%' },
+  { father: '1-2', name: '人工智能', percent: '5%' },
+  { father: '1-2', name: '人工智能', percent: '5%' },
+];
+
+export default [];
