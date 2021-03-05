a05d2c6f7d6a290319d35b816cd019893100f248
张鹤鸣 <13946592404@163.com>
2021-03-05 14:46:40 +0800

    [add] new el-switch

--- ui/src/components/requirement/EngineerCertification.vue
+++ ui/src/components/requirement/EngineerCertification.vue
@@ -6,25 +6,29 @@
       />
     </div>
     <div class="radioGroups">
+      <!-- edit button: collapse - show, course - show, auth - ok -->
       <el-switch
-        v-model="radioCollapse"
-        :active-text="$t('certification.switchGroups.collapseOpen')"
-        :inactive-text="$t('certification.switchGroups.collapseFold')"
-        active-color="#13ce66"
-        inactive-color="#ff4949"
-      />
-      <el-switch
-        v-model="radioCourse.value"
-        :active-text="$t('certification.switchGroups.courseOpen')"
-        :inactive-text="$t('certification.switchGroups.courseFold')"
-        :disabled="!radioCollapse"
-      />
-      <el-switch
-        v-if="radioPermission.show"
+        v-if="radioCollapse && radioCourse.value && radioPermission.show"
         v-model="radioPermission.value"
+        active-color="#E6A23C"
         :active-text="$t('certification.switchGroups.buttonShow')"
         :inactive-text="$t('certification.switchGroups.buttonHide')"
-        :disabled="!radioCollapse || !radioCourse.value"
+      />
+      <!-- course fold: collapse - show -->
+      <el-switch
+        v-if="radioCollapse"
+        v-model="radioCourse.value"
+        inactive-color="#909399"
+        :active-text="$t('certification.switchGroups.courseOpen')"
+        :inactive-text="$t('certification.switchGroups.courseFold')"
+      />
+      <!-- collapse fold: no limit -->
+      <el-switch
+        v-model="radioCollapse"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        :active-text="$t('certification.switchGroups.collapseOpen')"
+        :inactive-text="$t('certification.switchGroups.collapseFold')"
       />
     </div>
   </div>
