9f0a53d1fe32f0cc0a2dc83fe5c70ded6e59e6ff
13946592404 <13946592404@163.com>
2021-04-04 16:35:42 +0800

    [fix] tsconfig refer

--- ui/src/views/master/GraduateRequire.vue
+++ ui/src/views/master/GraduateRequire.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import EngineerCertification from '@/components/requirement/EngineerCertification.vue';
+import EngineerCertification from '../../components/requirement/EngineerCertification.vue';
 
 export default defineComponent({
   components: {
